b451eb32ce71221ec9dcba92507745870330b77a
Lusenti <rossetofr@gmail.com>
2021-09-19 21:10:21 -0300

    substituicao temporaria do banco pelo sqlite3

--- checklist/settings.py
+++ checklist/settings.py
@@ -77,16 +77,30 @@ WSGI_APPLICATION = 'checklist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# local PostgreSQL database
+# """ DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'checklistdb', 
+#         'USER': 'checklist_app', 
+#         'PASSWORD': 'checklist123',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# } """
+
+
+#sqlite3
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'checklistdb', 
-        'USER': 'checklist_app', 
-        'PASSWORD': 'checklist123',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
