55ce5502544a9cbe93b3c90f1db8ef94598435cb
pistesil <pistesil@gmail.com>
2021-10-18 22:10:30 -0300

    Adicionado tipo combobox

--- checklist/serializers.py
+++ checklist/serializers.py
@@ -37,7 +37,7 @@ class itemSerializer(serializers.HyperlinkedModelSerializer):
     grupo = serializers.CharField(source='grupo_fk.nome')
     class Meta:
         model = Item
-        fields = ['id', 'nome','descricao','modelo','grupo','itemtipo','valorpadrao']
+        fields = ['id', 'nome','descricao','modelo','grupo','itemtipo','valorpadrao','valor_choice']
 
 
 class modeloSerializer(serializers.HyperlinkedModelSerializer):
