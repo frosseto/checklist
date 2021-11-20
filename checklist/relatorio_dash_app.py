8fec22a24c8c8d577c59ff83af8f960d0f15b060
Lusenti <rossetofr@gmail.com>
2021-11-20 14:31:02 -0300

    pagina de relatorio

--- checklist/relatorio_dash_app.py
+++ checklist/relatorio_dash_app.py
@@ -1,55 +1,85 @@
-#import dash_core_components as dcc
 from dash import dcc
-#import dash_html_components as html
 from dash import html
+from dash import dash_table as dt
 from dash.dependencies import Input, Output
-import plotly.graph_objs as go
+import plotly.express as px
 from django_plotly_dash import DjangoDash
+
+from django.db import connection
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = DjangoDash('Relatorio', external_stylesheets=external_stylesheets)
 
+#df = pd.read_csv('https://git.io/Juf1t')
+query='SELECT * FROM public."ListaVerificacaoResultadoAnalise"'
+df = pd.read_sql_query(query, connection)
+
+resultado_query='''
+SELECT resultado_analise."Resposta", resultado_analise."Modelo", COUNT(*) AS "Qde" 
+FROM public."ListaVerificacaoResultadoAnalise" AS resultado_analise
+GROUP BY resultado_analise."Resposta",resultado_analise."Modelo"
+'''
+resultado_df = pd.read_sql_query(resultado_query, connection)
+
+status_query= '''
+ SELECT lista_analise."Status"
+ ,lista_analise."Modelo" 
+ ,COUNT(*) AS "Qde"
+ FROM public."ListaVerificacaoResultadoAnalise" lista_analise
+ GROUP BY lista_analise."Modelo", lista_analise."Status"
+'''
+status_df = pd.read_sql_query(status_query, connection)
 
 app.layout = html.Div([
-    html.H1('Square Root Slider Graph'),
-    dcc.Graph(id='slider-graph', animate=True, style={"backgroundColor": "#1a2d46", 'color': '#ffffff'}),
-    dcc.Slider(
-        id='slider-updatemode',
-        marks={i: '{}'.format(i) for i in range(20)},
-        max=20,
-        value=2,
-        step=1,
-        updatemode='drag',
+    html.Div([
+        dcc.Dropdown(
+                id="modelo-select",
+                options=[
+                    {'label': 'Encerramento de OMs', 'value': 'Encerramento de OMs'},
+                    {'label': 'Rotas', 'value': 'Rotas'},
+                ],
+                value='Encerramento de OMs'
+                ),       
+        html.Div([
+            html.Div([
+                html.H6('Qde por Resultado'),                
+                dcc.Graph(id='resultado-graph'),
+
+            ], className="six columns"),
+
+            html.Div([
+                html.H6('Qde por Status'),
+                dcc.Graph(id='status-graph'),
+            ], className="six columns"),
+        ], className="row"),
+        html.H6('Resultados por Lista de Verificação'),
+        dt.DataTable(
+        id='tbl', data=df.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in df.columns],
+        style_cell={'textAlign': 'left'},
     ),
+
+
+    ]),
+    
 ])
 
 
 @app.callback(
-               Output('slider-graph', 'figure'),
-              [Input('slider-updatemode', 'value')])
-def display_value(value):
+    Output('resultado-graph', 'figure'),
+    Input('modelo-select', 'value'))
+def update_figure(modelo):
+    fig = px.bar(resultado_df.loc[resultado_df['Modelo'] == modelo], x="Resposta", y="Qde")
+    fig.update_layout(transition_duration=500)
+    return fig
 
 
-    x = []
-    for i in range(value):
-        x.append(i)
-
-    y = []
-    for i in range(value):
-        y.append(i*i)
-
-    graph = go.Scatter(
-        x=x,
-        y=y,
-        name='Manipulate Graph'
-    )
-    layout = go.Layout(
-        paper_bgcolor='#27293d',
-        plot_bgcolor='rgba(0,0,0,0)',
-        xaxis=dict(range=[min(x), max(x)]),
-        yaxis=dict(range=[min(y), max(y)]),
-        font=dict(color='white'),
-
-    )
-    return {'data': [graph], 'layout': layout}
+@app.callback(
+    Output('status-graph', 'figure'),
+    Input('modelo-select', 'value'))
+def update_figure(modelo):
+    fig = px.bar(status_df.loc[status_df['Modelo'] == modelo], x="Status", y="Qde")
+    fig.update_layout(transition_duration=500)
+    return fig
